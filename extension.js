fc47639572e46cf6b2726d81ef5c0a09d32533c7
Ella <emmanuellaumoye18@gmail.com>
2023-02-15 11:46:47 -0500

    Button changes background to specified color. Title of note and note content is saved to a dict

--- extension.js
+++ extension.js
@@ -1,35 +1,51 @@
+var colorParent = document.querySelector(".color");
+var titleText = document.querySelector("#title_of_page")
+var redButton = colorParent.querySelector("#red");
+var yellowButton = colorParent.querySelector("#yellow");
+var greenButton = colorParent.querySelector("#green");
+var title_of_note = document.querySelector("#title_of_note")
+var notes_content = document.querySelector("#freeform")
+var notes_dict = {}
+var saveButton = colorParent.querySelector("#save")
+saveButton.addEventListener("click", onSaveButtonClicked)
 window.addEventListener("resize", function () {
     document.getElementById("freeform").style.width = (window.innerWidth)/2 + "px";
     document.getElementById("freeform").style.height = (window.innerHeight)/10 + "px";
 });
-var colorParent = document.querySelector(".color");
-console.log(colorParent)
-function onBackgroundButtonClicked (element_name) {
-    if(element_name.id === "red"){
-        console.log(element_name)
-        document.body.style.backgroundColor = "red";
-    }
 
-    else if(element_name.id === "green"){
-        document.body.style.backgroundColor = "green";
-    }
-
-    else{
-        document.body.style.backgroundColor = "yellow";
-
-    }
+function setBackgroundColor () {
+    titleText.style.color = "white";
 }
-var redButton;
-var yellowButton;
-var greenButton;
-function getElementIds(){
-    redButton = colorParent.querySelector("#red")
-    redButton.addEventListener("click", onBackgroundButtonClicked(redButton));
-    yellowButton = colorParent.querySelector("#yellow")
-    yellowButton.addEventListener("click", onBackgroundButtonClicked(yellowButton));
-    greenButton = colorParent.querySelector("#green") 
-    greenButton.addEventListener("click", onBackgroundButtonClicked(greenButton));
 
+// these changes the background color to the specified below when clicked
+redButton.addEventListener("click", function(){
+    document.body.style.backgroundColor = "red";
+    titleText.style.color = "white";
+});
+yellowButton.addEventListener("click", function(){
+    document.body.style.backgroundColor = "yellow";
+    titleText.style.color = "white";
+})
+greenButton.addEventListener("click", function(){
+    document.body.style.backgroundColor = "green";
+    setBackgroundColor();
+})
+
+// this checks whether the title of the text is empty
+
+
+// for now, user when the save button is clicked, the items should be added to a dictionary
+// where the title is the key, and the content is the value
+function onSaveButtonClicked(){
+    // first check if the title of note or the note space is empty
+    if (title_of_note.value === "" || notes_content.value === "") {
+        console.log("One of the input fields is empty, please fill them out")
+    }
+
+    // if it's not empty, save to a dictionary
+
+    notes_dict[title_of_note.value] = notes_content.value
+    console.log(notes_dict)
 }
 
 // getElementIds()

b93dd2a89598a280e36e05faae816dc3b4556fde
Ella <emmanuellaumoye18@gmail.com>
2023-03-06 13:18:57 -0500

    Currently, working on saving the data to chrome storage. It is still an ongoing issue

--- extension.js
+++ extension.js
@@ -7,6 +7,7 @@ var title_of_note = document.querySelector("#title_of_note")
 var notes_content = document.querySelector("#freeform")
 var tabName = document.title;
 const date = new Date();
+myNotes = []
 // Get a reference to the table
 var table = document.querySelector('table');
 
@@ -57,46 +58,70 @@ function onSaveButtonClicked(){
     // if it's not empty, save to a dictionary
 
     notes_dict.title = title_of_note.value
+    console.log(typeof(notes_dict.title))
     notes_dict.notes = notes_content.value
-    render(notes_dict)
-
-}
-function sortByDate(rows){
-    // get an array of all the rows in the table body
-    // To clarify, when the sort method is called on an array, it loops through all the elements in the array and compares adjacent pairs of elements. In our case, each element in the array is a row in the table, and the sort method compares the date values in the rows to determine their relative order.
-    // So, to be more accurate, a and b represent adjacent rows in the rows array that are 
-    // being compared.The sort method will continue comparing adjacent rows until all the rows 
-    // have been sorted in the correct order.
-
-    // sort the rows in descending order based on the date column
-    rows.sort(function (a, b) {
-        var dateA = new Date(a.querySelector("#date").textContent);
-        var dateB = new Date(b.querySelector("#date").textContent);
-        return dateB - dateA;
-    });
-    console.log("y")
-
+    addtoStorage()
 }
 
-function render (notes_dict) {
-    // Create a new row dynamically
-    var newRow = table.insertRow();
+// // if the leadsstorage aint empty
+// // then set my leads to the leadstorage
+// // which has all the leads up to this point
+// if (leadsFromLocalStorage) {
+//     myNotes = leadsFromLocalStorage
+//     render(myNotes)
+// }
 
-    // Add cells to the row
-    var tabNameCell = newRow.insertCell();
-    var titleCell = newRow.insertCell();
-    var dateCell = newRow.insertCell();
-    var notesCell = newRow.insertCell();
+// add to storage creates an object variable that contains
+// all the notes information
+function addtoStorage(){
+    
+    const NotesInfo = {
+        tabNameCell: tabName,
+        titleCell: notes_dict.title,
+        dateCell: date,
+        notesCell: notes_dict.notes
+    }
 
-    // Set the cell values
-    tabNameCell.textContent = tabName;
-    titleCell.textContent = notes_dict.title;
-    dateCell.textContent = date;
-    notesCell.textContent = notes_dict.notes;
-    notes_dict = {};
+    // we have to convert this object to a string for it to be saved in the storage
+    window.localStorage.setItem(myNotes, JSON.stringify(NotesInfo))
+    console.log(window.localStorage.getItem(myNotes))
+    render(myNotes)
     title_of_note.value = "";
     notes_content.value = "";
+}
+function render (notes) {
+    // Create a new row dynamically
 
+    // somehow notes is empty hm
+    console.log(window.localStorage.getItem(notes))
+    console.log(notes.length)
+    // notes is an object string not an array, so it has a length of 0
+    // first we need to convert it back to an object
+    const leadsFromLocalStorage = JSON.parse(localStorage.getItem(notes))
+    console.log(leadsFromLocalStorage)
+    if(leadsFromLocalStorage.length > 0){
+    for(let i = 0; i < leadsFromLocalStorage.length; i++){
+        var newRow = table.insertRow();
+
+        // Add cells to the row
+        var tabNameCell = newRow.insertCell();
+        var titleCell = newRow.insertCell();
+        var dateCell = newRow.insertCell();
+        var notesCell = newRow.insertCell();
+        console.log(notes)
+        tabNameCell = leadsFromLocalStorage[i];
+        console.log(tabNameCell)
+        // titleCell = notes[i].titleCell;
+        // dateCell = notes[i].dateCell;
+        // notesCell = notes[i].notesCell
+
+    }
+    
+    }
+    // Set the cell values in the object var NotesInfo
+    
+
+    
     // // sort the rows in descending order based on the date column
     // var table_size = document.getElementById("table").rows.length;
 
@@ -128,7 +153,21 @@ function render (notes_dict) {
     // rows.forEach(function (row) {
     //     tbody.appendChild(row);
     // });
+
+    // prev code
+    // dateCell.textContent = date;
+    // notesCell.textContent = notes_dict.notes;
+    // notes_dict = {};
+    // title_of_note.value = "";
+    // notes_content.value = "";
+
+    // notes_dict = {};
+    // title_of_note.value = "";
+    // notes_content.value = "";
 }
+
+// write now, I need code that will collect these 
+// items, save it to the local storage, and render it once the table is reloaded
 
 
 

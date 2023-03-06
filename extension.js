ddc7ca8e1dad76dbb889a86a74fa4034a310f193
Ella <emmanuellaumoye18@gmail.com>
2023-03-06 12:29:36 -0500

    The user can now render their note in a table format. All previously added notes will be at the top as the table is sorted in ascending order. That might be a potential issue for later so use can filter the table to display results how they want it

--- extension.js
+++ extension.js
@@ -5,6 +5,12 @@ var yellowButton = colorParent.querySelector("#yellow");
 var greenButton = colorParent.querySelector("#green");
 var title_of_note = document.querySelector("#title_of_note")
 var notes_content = document.querySelector("#freeform")
+var tabName = document.title;
+const date = new Date();
+// Get a reference to the table
+var table = document.querySelector('table');
+
+
 var notes_dict = {
     title : "",
     notes : ""
@@ -52,32 +58,78 @@ function onSaveButtonClicked(){
 
     notes_dict.title = title_of_note.value
     notes_dict.notes = notes_content.value
-    console.log(notes_dict)
-    localStorage.setItem("notes_dict", JSON.stringify(notes_dict))
     render(notes_dict)
 
 }
-// chrome.runtime.onInstalled.addListener(function () {
-//     console.log('Extension installed!');
+function sortByDate(rows){
+    // get an array of all the rows in the table body
+    // To clarify, when the sort method is called on an array, it loops through all the elements in the array and compares adjacent pairs of elements. In our case, each element in the array is a row in the table, and the sort method compares the date values in the rows to determine their relative order.
+    // So, to be more accurate, a and b represent adjacent rows in the rows array that are 
+    // being compared.The sort method will continue comparing adjacent rows until all the rows 
+    // have been sorted in the correct order.
 
-//     // Initialize your extension here, including setting up the chrome.storage API
-// });
+    // sort the rows in descending order based on the date column
+    rows.sort(function (a, b) {
+        var dateA = new Date(a.querySelector("#date").textContent);
+        var dateB = new Date(b.querySelector("#date").textContent);
+        return dateB - dateA;
+    });
+    console.log("y")
 
-// this method adds the note to the list
-function render (notes_dict) {
-    console.log("hello")
-    localStorage.setItem("notes_dict", JSON.stringify(notes_dict))
-    // localStorage.getItem(['notes'], function(result){
-    //     var notes = result.notes || []
-    //     notes.push(note)
-
-    //     localStorage.setItem({notes: notes}, function(){
-    //         console.log("Note saved")
-    //     })
-    // })
-    console.log("yeah")
-    localStorage.getItem(notes_dict)
 }
+
+function render (notes_dict) {
+    // Create a new row dynamically
+    var newRow = table.insertRow();
+
+    // Add cells to the row
+    var tabNameCell = newRow.insertCell();
+    var titleCell = newRow.insertCell();
+    var dateCell = newRow.insertCell();
+    var notesCell = newRow.insertCell();
+
+    // Set the cell values
+    tabNameCell.textContent = tabName;
+    titleCell.textContent = notes_dict.title;
+    dateCell.textContent = date;
+    notesCell.textContent = notes_dict.notes;
+    notes_dict = {};
+    title_of_note.value = "";
+    notes_content.value = "";
+
+    // // sort the rows in descending order based on the date column
+    // var table_size = document.getElementById("table").rows.length;
+
+    // if (table_size > 0){
+    //     var rows = Array.from(document.querySelector("tbody").querySelectorAll("tr"));
+
+    //     rows.sort(function (a, b) {
+    //         if (table_size > 1){
+    //             var dateA = new Date(a.querySelector("#date").textContent);
+    //             var dateB = new Date(b.querySelector("#date").textContent);
+    //             return dateB - dateA;
+
+    //         }
+
+    //         else{
+    //             return
+    //         }
+            
+    //     });
+    //     }
+
+    // // remove all rows from the table body
+    // var tbody = document.querySelector("tbody");
+    // while (tbody.firstChild) {
+    //     tbody.removeChild(tbody.firstChild);
+    // }
+
+    // // insert the sorted rows back into the table body in the correct order
+    // rows.forEach(function (row) {
+    //     tbody.appendChild(row);
+    // });
+}
+
 
 
 

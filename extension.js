49b9fe119c14c0346e971afdd2f4801352a2644a
Ella <emmanuellaumoye18@gmail.com>
2023-03-27 21:58:58 -0400

    Created a modal dialog box so that whenever the user clikcs on a row, they can preview the full notes, date taken, and title

--- extension.js
+++ extension.js
@@ -9,10 +9,14 @@ var notes_content = document.querySelector("#freeform")
 var tabName = document.title;
 const date = new Date();
 myNotes = []
-var table = document.querySelector('table');
+var notes_table = document.querySelector('table');
 var saveButton = colorParent.querySelector("#save")
 var deleteButton = colorParent.querySelector("#delete")
-
+var modal_box = document.querySelector(".modal")
+var modal_close = modal_box.querySelector("#close_btn")
+var modal_note = modal_box.querySelector("#note-content")
+var modal_title = modal_box.querySelector("#note-title")
+var modal_date  = modal_box.querySelector("#date_of_note")
 // creates a dictionary for the notes inc title and notes
 var notes_dict = {
     title : "",
@@ -55,18 +59,11 @@ function onSaveButtonClicked(){
         console.log("One of the input fields is empty, please fill them out")
         alert("One of the input fields is empty")
         return
-        // if(title_of_note.value === ""){
-        //     title_of_note.style.backgroundColor = "red";
-        // }
-
-        // if(notes_content.value === ""){
-        //     notes_content.style.backgroundColor = "red";
-
-        // }
     }
 
     notes_dict.title = title_of_note.value
     notes_dict.notes = notes_content.value
+    // console.log(notes_dict)
     addtoStorage()
 }
 
@@ -76,20 +73,22 @@ function addtoStorage(){
         tabNameCell: tabName,
         titleCell: notes_dict.title,
         notesCell: notes_dict.notes,
-        dateCell: date
+        dateCell: date.toDateString()
     }
     myNotes.push(NotesInfo)
     window.localStorage.setItem("myNotes", JSON.stringify(myNotes))
+    // console.log(notes_dict)
     title_of_note.value = "";
     notes_content.value = "";
 }
 
+// deleteButton should clear out the table, it doesn't work yet
 deleteButton.addEventListener("dblclick", function () {
     localStorage.clear()
     myLeads = []
     render(myLeads)
 })
-
+// obtains the notes from the local storage
 const notesFromLocalStorage = JSON.parse(window.localStorage.getItem("myNotes"))
 if (notesFromLocalStorage) {
     myNotes = notesFromLocalStorage
@@ -100,9 +99,11 @@ if (notesFromLocalStorage) {
 function render (notes) {
     for(let i = 0; i < notes.length; i++){
         var notes_object = notes[i]
-        console.log(notes_object)
+        // console.log(notes_dict)
         // Assuming "table" is a reference to the <table> element
         var newRow = document.createElement("tr");
+        // assigning an id to the row
+        newRow.setAttribute("data-id", i)
         // Create a new cell for the tabs text
         var tabNameCell = document.createElement("td");
         var tabNameText = document.createTextNode(notes_object.tabNameCell);
@@ -126,15 +127,67 @@ function render (notes) {
         // Sets the values of the table cell
         tabNameCell.textContent = notes_object.tabNameCell;
         titleCell.textContent = notes_object.titleCell;
-        noteCell.textContent = notes_object.notesCell;
+        // Only show the first 50 characters of the notes text
+        noteCell.textContent = notes_object.notesCell.substring(0, 100) + "...";
+        // console.log(notes_dict) 
+        noteCell.setAttribute("data-fullnotes", notes_dict.notes); // Set the full notes text as a data attribute
         dateCell.textContent = notes_object.dateCell;
-
         newRow.appendChild(tabNameCell);
         newRow.appendChild(titleCell);
         newRow.appendChild(noteCell);
         newRow.appendChild(dateCell);
 
         // Append the new row to the table
-        table.appendChild(newRow);
+        notes_table.appendChild(newRow);
     }
 }
+
+// this method adds an event listener to the notes_table
+// creates a modal dialog box with the notes in full display
+notes_table.addEventListener("click", function(event){
+    const row = event.target.closest("tr")
+    if (row){
+        const noteId = row.getAttribute("data-id"); // Get the unique identifier
+        var note = getNotesContent(noteId)
+        // console.log(note)
+        document.getElementById('modal').style.display = 'block';
+        // console.log(modal_note)
+        // console.log(row.cells[2].getAttribute("data-fullnotes"))
+        console.log(note.notesCell)
+        modal_note.innerText = note.notesCell
+
+        // console.log(modal_note)
+        modal_title.innerText = note.titleCell
+        modal_date.innerText = note.dateCell
+
+    }
+})
+
+// this method returns the notes at the specified row id
+function getNotesContent(noteId){
+    var note = myNotes[noteId]
+    return note
+
+}
+
+// this function blurs out the table
+var myBlurFunction = function (state) {
+    /* state can be 1 or 0 */
+    var containerElement = document.getElementById('main_container');
+    var overlayEle = document.getElementById('overlay');
+
+    if (state) {
+        overlayEle.style.display = 'block';
+        containerElement.setAttribute('class', 'blur');
+    } else {
+        overlayEle.style.display = 'none';
+        containerElement.setAttribute('class', null);
+    }
+};
+// this hids the modal dialog box when clicked
+modal_box.addEventListener("click", function(event){
+    modal_box.style.display = "none"
+})
+modal_close.addEventListener("click", function(event){
+    modal_box.style.display = "none"
+})
